c0ddc81873301ea8c691926c02ecd25a36668a5c
kibi <kmerakeb@gmail.com>
2019-02-09 12:33:49 -0800

    added Siteamps to the posts

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,17 @@
 from django.conf.urls import url
 from . import views
 from django.urls import path
+from django.contrib.sitemaps.views import sitemap
+from blog.sitemaps import PostSitemap
+
+sitemaps = {
+    'posts' : PostSitemap
+}
 
 urlpatterns =[
     path('', views.post_list_view, name='post_list_view'),
-    path('<int:year>)/<int:month>/<int:day>/<slug:post>/', views.post_detail_view, name='post_detail_view')
+    path('<int:year>)/<int:month>/<int:day>/<slug:post>/', views.post_detail_view, name='post_detail_view'),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps},
+         name='django.contrib.sitemaps.views.sitemap')
 
 ]
--- oplash/settings.py
+++ oplash/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sitemaps',
+    'django.contrib.sites',
     'blog',
 ]
 
@@ -119,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+SITE_ID = 1
